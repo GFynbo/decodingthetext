c93da2c38d9f1fe166c4b02697fab11270947c99
Gavin Fynbo <gavfynbo@gmail.com>
2017-03-25 11:19:08 -0400

    added flask and features

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import indicoio
-indicoio.config.api_key = '43f08f36ac7e0871c28d67f797a78aba'
+from flask import Flask
+from flask import render_template
 
-# single example
-indicoio.sentiment_hq("I love writing code!")
+app = Flask(__name__)
 
-text = input("Enter text here: ")
+@app.route("/")
+def home():
+    return render_template('index.html')
 
-# batch examp
-happiness = indicoio.sentiment_hq([
-    text
-])
+@app.route("/decode/")
+def decode():
+    return render_template('decode.html')
 
-print(happiness)
+if __name__ == "__main__":
+    app.run()
+
+'''
+def main():
+
+    hello()
+
+    indicoio.config.api_key = '43f08f36ac7e0871c28d67f797a78aba'
+
+    # single example
+    indicoio.sentiment_hq("I love writing code!")
+
+    text = input("Enter text here: ")
+
+    # batch examp
+    happiness = indicoio.sentiment_hq([
+        text
+    ])
+
+    print(happiness)
+    ans = "Result: " + str(happiness)
+    return ans
+'''
