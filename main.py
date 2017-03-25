8f3ade94e8f305b92e90e301cb8258511200cd1f
Tatiana Diaz-Gallegos <todg@bu.edu>
2017-03-25 13:37:54 -0400

    Updating Emoji Processing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import indicoio
 indicoio.config.api_key = '43f08f36ac7e0871c28d67f797a78aba'
 
+################################################################################
 # Indico Examples:
 #
 # Emotion: emotion(data, [api_key], [cloud], [top_n], [threshold])
@@ -16,79 +17,150 @@ indicoio.config.api_key = '43f08f36ac7e0871c28d67f797a78aba'
 #       Outputs: number between 0 and 1. This number is a probability representing
 #        the likelihood that the analyzed text is positive or negative
 #
+################################################################################
+# Emoji Unicode List:
 
-#User interface input
+# Emotions
+Sad = "\U0001F622"
+Fear = "\U0001F628"
+Happy = "\U0001F600"
+Angry = "\U0001F621"
+Surprise = "\U0001F631"
+
+# Personas
+# Logistician = "\U0001F #
+# Consul = "\U0001F  #
+# Protagonist = "\U0001F
+Advocate = "\U0001F575" #investigator
+# Logician = "\U0001F
+Adventurer = "\U0001F3C4" #surfing
+Mediator = "\U00002696"  #judge
+# Entrepreneur = "\U0001F
+Architect = "\U0001F477"  #hardhat
+#Virtuoso = "\U0001F" #graduate
+# Campaigner = "\U0001F
+Defender = "\U0001F46E" #cop
+# Debater = "\U0001F #teacher
+# Commander = "\U0001F
+Executive = "\U0001F4BC"  #suit
+Entertainer = "\U0001F3A4" #singer
+
+print("Entertainer",Entertainer)
+print("Executive",Executive)
+print("Defender",Defender)
+print("Architect",Architect)
+print("Mediator",Mediator)
+print("Adventurer",Adventurer)
+print("Advocate",Advocate)
+
+################################################################################
+
+# User interface input
 text = input("Enter the text message here: ")
 
-#Variables
+# Variables
 persona1 = indicoio.personas(text)
 sentiment1 = indicoio.sentiment_hq(text)
 emotion1 = indicoio.emotion(text)
 
-#Sentiment Message
+################################################################################
+
+# Sentiment Message
 if sentiment1 >= 0.5:
     print("\nThis looks like a good sign!")
 else:
-    print("Uh oh")
+    print("\nUh oh, this message isn't super positive.")
 
-#Emotion Message
-if emotion1['joy'] >= 0.5:
-    print("They seem pretty joyful!")
-if emotion1['anger'] >= 0.5:
-    print("They seem pretty angry...")
-if emotion1['fear'] >= 0.5:
-    print("They seem pretty fearful.")
-if emotion1['surprise'] >= 0.5:
-    print("They seem pretty surprised!")
-if emotion1['sadness'] >= 0.5:
-    print("They seem pretty sad...")
+################################################################################
 
-#Finding Max probability of Emotion
-tempe = 0
-tempe_str = ""
-for i in emotion1:
-    if emotion1[i] > tempe:
-        tempe = emotion1[i]
-        tempe_str = i
+# Emotion Message
+printed_em = False # check make sure not print max if printed an emotion already
+second_pr = False # check if printed more than one message, so prints accordingly
 
-print("They seem like they are feeling some",tempe_str, ".")
+if emotion1['joy'] >= 0.3:
+    print("They seem pretty joyful!",Happy)
+    printed_em = True
+    second_pr = True
+if emotion1['anger'] >= 0.3:
+    if second_pr == True:
+        print("And, they also seem pretty angry.",Angry)
+        printed_em = True
+    else:
+        print("They seem pretty angry.",Angry)
+        printed_em = True
+        second_pr = True
+if emotion1['fear'] >= 0.3:
+    if second_pr == True:
+        print("And, they also seem pretty fearful.",Fear)
+        printed_em = True
+    else:
+        print("They seem pretty fearful.",Fear)
+        printed_em = True
+        second_pr = True
+if emotion1['surprise'] >= 0.3:
+    if second_pr == True:
+        print("And, they also seem pretty surprised!",Surprise)
+        printed_em = True
+    else:
+        print("They seem pretty surprised!",Surprise)
+        printed_em = True
+        second_pr = True
+if emotion1['sadness'] >= 0.3:
+    if second_pr == True:
+        print("And, they also seem pretty sad.",Sad)
+        printed_em = True
+    else:
+        print("They seem pretty sad.",Sad)
+        printed_em = True
 
+# Finding Max probability of Emotion
+if printed_em ==  False:
+    tempe = 0
+    tempe_str = ""
+    for i in emotion1:
+        if emotion1[i] > tempe:
+            tempe = emotion1[i]
+            tempe_str = i
 
-#Persona message
+    print("They seem like they are feeling some",tempe_str, ".")
+
+################################################################################
+
+# Persona message
 if persona1['logistician'] >= 0.5:
-    print("Looks like you are dealing with a logistician\n")
+    print("It looks like you are dealing with a logistician\n")
 elif persona1['consul'] >= 0.5:
-    print("Looks like you are dealing with a consul\n")
+    print("It looks like you are dealing with a consul\n")
 elif persona1['protagonist'] >= 0.5:
-    print("Looks like you are dealing with a protagonist\n")
+    print("It looks like you are dealing with a protagonist\n")
 elif persona1['advocate'] >= 0.5:
-    print("Looks like you are dealing with a advocate\n")
+    print("It looks like you are dealing with a advocate\n")
 elif persona1['logician'] >= 0.5:
-    print("Looks like you are dealing with a logician\n")
+    print("It looks like you are dealing with a logician\n")
 elif persona1['adventurer'] >= 0.5:
-    print("Looks like you are dealing with a adventurer\n")
+    print("It looks like you are dealing with a adventurer\n")
 elif persona1['mediator'] >= 0.5:
-    print("Looks like you are dealing with a mediator\n")
+    print("It looks like you are dealing with a mediator\n")
 elif persona1['entrepreneur'] >= 0.5:
-    print("Looks like you are dealing with a entrepreneur\n")
+    print("It looks like you are dealing with a entrepreneur\n")
 elif persona1['architect'] >= 0.5:
-    print("Looks like you are dealing with a architect\n")
+    print("It looks like you are dealing with a architect\n")
 elif persona1['virtuoso'] >= 0.5:
-    print("Looks like you are dealing with a virtuoso\n")
+    print("It looks like you are dealing with a virtuoso\n")
 elif persona1['campaigner'] >= 0.5:
-    print("Looks like you are dealing with a campaigner\n")
+    print("It looks like you are dealing with a campaigner\n")
 elif persona1['defender'] >= 0.5:
-    print("Looks like you are dealing with a defender\n")
+    print("It looks like you are dealing with a defender\n")
 elif persona1['debater'] >= 0.5:
-    print("Looks like you are dealing with a debater\n")
+    print("It looks like you are dealing with a debater\n")
 elif persona1['commander'] >= 0.5:
-    print("Looks like you are dealing with a commander\n")
+    print("It looks like you are dealing with a commander\n")
 elif persona1['executive'] >= 0.5:
-    print("Looks like you are dealing with a executive\n")
+    print("It looks like you are dealing with a executive\n")
 elif persona1['entertainer'] >= 0.5:
-    print("Looks like you are dealing with a entertainer\n")
+    print("It looks like you are dealing with a entertainer\n")
 
-#Finding Max probability of Persona
+# Finding Max probability of Persona
 temp = 0
 temp_str = ""
 for i in persona1:
@@ -96,7 +168,9 @@ for i in persona1:
         temp = persona1[i]
         temp_str = i
 
-print("Looks like you are dealing with a(n)",temp_str, ".\n")
+print("It looks like you are dealing with a(n)",temp_str, ".\n")
+
+################################################################################
 
 # print('Emotion right now:', emotion1)
 # print('Sentiment right now:', sentiment1)
